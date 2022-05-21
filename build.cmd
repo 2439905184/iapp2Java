33c1d9a1e488beb58852a94fe5366db78ef8ebe6
2439905184 <suchenglong018@126.com>
2022-05-21 20:24:24 +0800

    🔧 build(新增用于半自动编译的批处理文件): 构建系统更新
    新增用于半自动编译的批处理文件，将构建目录移动至test_compile

---
+++ build.cmd
@@ -0,0 +1,4 @@
+mkdir test_compile
+python compile.py "iyu_src/shortCut.iyu" "test_compile/shortCut.java"
+python compile.py "iyu_src/test.iyu" "test_compile/test.java"
+python compile.py "iyu_src/zhushi.iyu" "test_compile/zhushi.java"
