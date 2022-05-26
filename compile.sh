2887c53a474442b176454f19daca7582264456c6
2439905184 <suchenglong018@126.com>
2022-05-26 20:09:17 +0800

    🐎 ci: 添加编译测试

---
+++ compile.sh
@@ -0,0 +1,5 @@
+mkdir test_compile
+python compile.py "iyu_src/shortCut.iyu" "test_compile/shortCut.java"
+python compile.py "iyu_src/test.iyu" "test_compile/test.java"
+python compile.py "iyu_src/zhushi.iyu" "test_compile/zhushi.java"
+python compile.py "iyu_src/manyJava.iyu" "test_compile/manyJava.java"
